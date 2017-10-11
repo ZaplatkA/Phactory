2b1a17ba9714ba8f59b52b7df6a3163daa213d61
Accismus <accismus@mail.ru>
2017-10-11 12:24:55 +0300

    SomeGoods class added

--- Phactory/Program.cs
+++ Phactory/Program.cs
@@ -10,6 +10,7 @@ namespace Phactory
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ Phactory/SomeGoods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phactory
+{
+    class SomeGoods
+    {
+        private string name;
+        private double price;
+
+        SomeGoods(string nameVal, double priceVal)
+        {
+            name = nameVal;
+            price = priceVal;
+        }
+
+        public string Name()
+        {
+            return name;
+        }
+
+        public double Price()
+        {
+            return price;
+        }
+    }
+}
